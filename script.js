08e07f12b4f8e63586bdd12b6eb2dd1f4998ef1e
drooler <85669046+bijenmanlol@users.noreply.github.com>
2024-03-05 11:57:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let slider = document.getElementById("slider");
 slider.oninput = function() {
   document.getElementById("paki").style.animationDuration = (1001 - this.value) + "ms";
 }
+
+function combine() {
+  document.getElementById("result").innerHTML = "Uw volledige naam is: " + document.getElementById("first").value.charAt(0).toUpperCase() + document.getElementById("first").value.substring(1).toLowerCase() + " " + document.getElementById("last").value.charAt(0).toUpperCase() + document.getElementById("last").value.substring(1).toLowerCase();
+}
